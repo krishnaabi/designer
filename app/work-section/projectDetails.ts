cc95fc05a20fe1fb5201fe976e3a145839c13c85
krishnaabi <101504990+krishnaabi@users.noreply.github.com>
2026-04-08 16:23:15 +0530

    Update projectDetails.ts

--- app/work-section/projectDetails.ts
+++ app/work-section/projectDetails.ts
@@ -16,7 +16,7 @@ export const devProjects = [
     description:
       "Hanioo is a user-friendly platform connecting interpreters and clients for seamless communication, transparent booking, and secure payment management..",
     technologies: ["Figma", "UI/UX Design", "Julius AI", "Mira"],
-    figma: "https://www.figma.com/proto/GGLyNXbkrHmvXWZD4ks84u/Case-Study-Template---Creative-Ferry--Community-?node-id=206-28&p=f&t=bNPPsvK9YMMsa2JD-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1",
+    figma: "https://www.figma.com/design/GGLyNXbkrHmvXWZD4ks84u/Case-Study-Template---Creative-Ferry--Community-?node-id=282-58&t=kfPGs3Q4J4XZLMzl-1",
     image: require(".//../../public/0.jpg"),
     available: true,
     demo: "",
@@ -54,73 +54,6 @@ export const devProjects = [
     image: require(".//../../public/1.jpg"),
     available: true,
   },
-  {
-    id: 4,
-    name: "YumAI",
-    description:
-      "App design for YumAI, an AI-powered food and recipe app that personalizes meal suggestions based on user preferences, health goals, and dietary needs.",
-    technologies: ["Figma", "UI/UX Design", "Mobile App"],
-    figma: "https://www.figma.com/design/thcUgBZtNexGKTWqLY25C1/YumAI-App?m=auto&t=PwKQoFFVfbWALXbw-1",
-    demo: "https://www.behance.net/gallery/209806095/YumAI",
-    image: require(".//../../public/5.png"),
-    available: true,
-  },
-  // {
-  //   id: 4,
-  //   name: "SkyWatch",
-  //   description:
-  //     "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current weather and forecast for the next 2 days in any city.",
-  //   technologies: ["React", "CSS", "Chart.js"],
-  //   github: "https://github.com/victorcodess/weather-forecast-website",
-  //   demo: "https://sky-watch.vercel.app/",
-  //   image: require(".//../../public/projects/skywatch-flip.png"),
-  //   available: true,
-  // },
-  // {
-  //   id: 4,
-  //   name: "Alpaca Image Generator",
-  //   description:
-  //     "An image generator website that allows users to generate, combine, and download images.",
-  //   technologies: ["React", "CSS", "Merge Images"],
-  //   github: "https://github.com/victorcodess/alpaca-image-generator",
-  //   demo: "http://alpaca-image-generator-beta.vercel.app",
-  //   image: require(".//../../public/projects/alpaca-flip.png"),
-  //   available: true,
-  // },
-
-  // {
-  //   id: 5,
-  //   name: "Link Shortener",
-  //   description:
-  //     "A website that reduces the length of your URL using Bit.ly's API",
-  //   technologies: ["JavaScript", "CSS", "Bit.ly's API"],
-  //   github: "https://github.com/victorcodess/url-shortener",
-  //   demo: "https://url-shortener-nine-delta.vercel.app",
-  //   image: require(".//../../public/projects/shortener-new.webp"),
-  //   available: true,
-  // },
-  // {
-  //   id: 6,
-  //   name: "Carpooling Service",
-  //   description:
-  //     "TMTM helps Covenant University students find fellow students who are headed to the same location, so they can share a ride and split the cost.",
-  //   technologies: ["Material UI", "React", "Formik"],
-  //   github: "https://github.com/victorcodess/carpooling-service",
-  //   demo: "",
-  //   image: require(".//../../public/projects/carpool-new.webp"),
-  //   available: false,
-  // },
-  // {
-  //   id: 7,
-  //   name: "MLSC.ng",
-  //   description:
-  //     "This is platform for Microsoft Learn Student Ambassadors to shorten links, append their sharing IDs and generate event certificates.",
-  //   technologies: ["Next.js", "Next Auth", "Tailwind CSS"],
-  //   github: "https://github.com/msp-nigeria/mlsc.ng-frontend",
-  //   demo: "",
-  //   image: require(".//../../public/projects/mlsc.png"),
-  //   available: false,
-  // },
 ];
 
 export const designProjects = [
